ffca284d614e79b2569c7cb1875678e4ed0583cb
BRP7 <bhumiparmar0007@gmail.com>
2024-12-12 22:13:37 +0530

    footer component

---
+++ 03Web/src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="w-full bg-darkGray text-lightGray py-6 text-center">
+      <p>© 2024 NxtSite. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
